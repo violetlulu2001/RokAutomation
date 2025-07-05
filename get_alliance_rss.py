9181acc6125e85289a236624f13c91817ce8ad6b
violetlulu2001 <stroeandrei483@gmail.com>
2025-07-05 15:09:27 +0300

    Add Json file and alliance rss

--- get_alliance_rss.py
+++ get_alliance_rss.py
@@ -5,13 +5,13 @@ import pyautogui
 
 
 if __name__ == '__main__':
-    count_accounts = len(accounts["GoldBot"])
+    count_accounts = len(accounts)
     for i in range(1, count_accounts+1):
         pyautogui.click(setting['Profile']["x"], setting['Profile']["y"], duration=1)
         pyautogui.click(setting['Setting']["x"], setting['Setting']['y'], duration=1)
         pyautogui.click(setting['Account']['x'], setting['Account']['y'], duration=1)
-        pyautogui.click(accounts[f'Bot{i}']['x'], accounts[f'TepBot{i}']['y'], duration=1)
-        pyautogui.click(accounts[f'Bot{i}']['x'], accounts[f'TepBot{i}']['y'], duration=1)
+        pyautogui.click(accounts[f'Bot{i}']['x'], accounts[f'Bot{i}']['y'], duration=1)
+        pyautogui.click(accounts[f'Bot{i}']['x'], accounts[f'Bot{i}']['y'], duration=1)
         pyautogui.click(setting['Account_changer']['yes_btn_change_acc']['x'], setting['Account_changer']['yes_btn_change_acc']['y'], duration=1)
         time.sleep(10)
         pyautogui.click(setting['Account_changer']['yes_btn_change_acc']['x'], setting['Account_changer']['yes_btn_change_acc']['y'], duration=1)
