f2fc286a5ede4466454e52dc6f23a0435289c664
violetlulu2001 <stroeandrei483@gmail.com>
2025-07-05 14:47:08 +0300

    Add Json file and alliance rss

---
+++ get_alliance_rss.py
@@ -0,0 +1,28 @@
+import time
+
+from reader_json import accounts, setting, rightmenu
+import pyautogui
+
+
+if __name__ == '__main__':
+    count_accounts = len(accounts["GoldBot"])
+    for i in range(1, count_accounts+1):
+        pyautogui.click(setting['Profile']["x"], setting['Profile']["y"], duration=1)
+        pyautogui.click(setting['Setting']["x"], setting['Setting']['y'], duration=1)
+        pyautogui.click(setting['Account']['x'], setting['Account']['y'], duration=1)
+        pyautogui.click(accounts[f'Bot{i}']['x'], accounts[f'TepBot{i}']['y'], duration=1)
+        pyautogui.click(accounts[f'Bot{i}']['x'], accounts[f'TepBot{i}']['y'], duration=1)
+        pyautogui.click(setting['Account_changer']['yes_btn_change_acc']['x'], setting['Account_changer']['yes_btn_change_acc']['y'], duration=1)
+        time.sleep(10)
+        pyautogui.click(setting['Account_changer']['yes_btn_change_acc']['x'], setting['Account_changer']['yes_btn_change_acc']['y'], duration=1)
+        time.sleep(10)
+        pyautogui.click(rightmenu['Alliance']["location_btn"]['x'], rightmenu['Alliance']["location_btn"]['y'], duration=1)
+        pyautogui.click(rightmenu['Alliance']['Alliance_list']['territory']["location_btn"]['x'], rightmenu['Alliance']['Alliance_list']['territory']["location_btn"]['y'], duration=1)
+        pyautogui.click(rightmenu['Alliance']['Alliance_list']['territory']["claim"]['x'], rightmenu['Alliance']['Alliance_list']['territory']["claim"]['y'], duration=1)
+        pyautogui.click(rightmenu['Alliance']['Alliance_list']['territory']["close_teritory"]['x'], rightmenu['Alliance']['Alliance_list']['territory']["close_teritory"]['y'], duration=1)
+        pyautogui.click(rightmenu['Alliance']['Alliance_list']['territory']["close_teritory"]['x'], rightmenu['Alliance']['Alliance_list']['territory']["close_teritory"]['y'], duration=1)
+
+
+
+
+
---
+++ reader_json.py
@@ -0,0 +1,17 @@
+import json
+import os
+
+path = os.path.join("RokAutomation", "src", "json_file")
+path = f'F:\\{path}'
+
+with open(os.path.join(path, "Accounts.json"), 'r') as account_reader:
+    accounts = json.load(account_reader)
+
+with open(os.path.join(path, "Settings.json"), 'r') as account_reader:
+    setting = json.load(account_reader)
+
+with open(os.path.join(path, "rightMenu.json"), 'r') as account_reader:
+    rightmenu = json.load(account_reader)
+
+
+
