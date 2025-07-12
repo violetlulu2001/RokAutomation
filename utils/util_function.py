ca0803bbc2c810e2c2e94bd27309b357414d3312
violetlulu2001 <stroeandrei483@gmail.com>
2025-07-13 02:06:04 +0300

    small fix

--- utils/util_function.py
+++ utils/util_function.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 def get_datetime_format() -> str:
     now = datetime.now()
-    return now.strftime("%Y%m%d_%H:%M:%S:%f")
+    return now.strftime("%Y%m%d_%H%M%S%f")
 
 def default_text(name_script:str, name:str , x_coord: str, y_coord: str):
     return f"[{get_datetime_format().split("_")[-1]}] {name_script}.py | Press {name} on coordinate X:{x_coord}, Y:{y_coord}\\n"
