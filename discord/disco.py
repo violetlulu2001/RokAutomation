bc97788883b97c245a6195e1c1d884210f3c05f8
violetlulu2001 <stroeandrei483@gmail.com>
2025-07-13 09:57:20 +0300

    small fix

--- discord/disco.py
+++ discord/disco.py
@@ -1,11 +1,24 @@
 import discord
 import os
+import random
+
 class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
 
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
+        if message.content == '!random_msg':
+            messages = [
+                "Hello there!",
+                "How's everyone doing?",
+                "Have a great day!",
+                "Time for some fun!",
+                "Discord is awesome!"
+            ]
+            await message.channel.send(random.choice(messages))
+
+
 
 intents = discord.Intents.default()
 intents.message_content = True
