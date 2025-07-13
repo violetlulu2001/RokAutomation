1533f5487d70592695b9984fe88c351258d1d1fa
lulu <45490904+violetlulu2001@users.noreply.github.com>
2025-07-13 16:02:25 +0300

    fix intend if

--- utils/os_zone.py
+++ utils/os_zone.py
@@ -3,17 +3,11 @@ from utils.util_function import get_datetime_format
 
 class OsFile:
     def __init__(self):
-        self.path_file = os.path.join("D:","RokAutomation", "logs", "violeta_logs")
+        self.path_file = os.path.join("D:", "logs", "violeta_logs")
 
     def create_folder(self)-> str | None:
-        current_logs = os.path.join(self.path_file, get_datetime_format().split("_")[0])
-        if not os.path.exists(current_logs):
-            os.mkdir(current_logs)
-        else:
-            current_logs = os.path.join(current_logs, get_datetime_format().split("_")[-1])
-            if not os.path.exists(current_logs):
-                os.mkdir(current_logs)
-                return current_logs
+        current_logs = os.path.join(self.path_file, get_datetime_format().split("_")[-1])
+        return current_logs
 
 
 os_file = OsFile()
