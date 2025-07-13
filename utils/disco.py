ad7bfc05b4784c9707fe0b5f3177de65fa7dc921
lulu <45490904+violetlulu2001@users.noreply.github.com>
2025-07-13 17:02:40 +0300

    fix intend if

--- utils/disco.py
+++ utils/disco.py
@@ -5,7 +5,7 @@ class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
         channel = self.get_channel(1393861723647377528)
-        file_path = r'D:\RokAutomation\logs\violeta_logs\donate_techs_rss_163653653215.txt'
+        file_path = r'D:\logs\violeta_logs\donate_techs_rss_163653653215.txt'
         if channel and os.path.exists(file_path):
             await channel.send(file=discord.File(file_path))
         else:
