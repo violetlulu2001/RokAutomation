b235c40688c44b29a79b188e4e0dd55ae2f3c0f7
lulu <45490904+violetlulu2001@users.noreply.github.com>
2025-07-13 16:09:24 +0300

    fix intend if

--- donate_techs_rss.py
+++ donate_techs_rss.py
@@ -12,12 +12,11 @@ if __name__ == '__main__':
     count_accounts = accounts["Accounts_count"]["first_acc"]
     path_file = os_file.create_folder()
     path_file: str = os.path.join(path_file, name_donate)
-    i = 0
     with open(f'{path_file}_{get_datetime_format().split("_")[-1]}.txt', 'a') as writer:
-        # for i in range(1, count_accounts+1):
-        writer.write(f'[{get_datetime_format().split("_")[-1]}] - {name_donate}.py | Changed account Lulu{i}\n')
-        writer.write(delimtator)
-        # change_city(i, writer)
-        writer.write(delimtator)
-        writer.write(f'[{get_datetime_format().split("_")[-1]}] - {name_donate}.py | Donate Alliance Techs Lulu{i}\n')
-        donate_alliance_tech(writer)
+        for i in range(1, count_accounts+1):
+            writer.write(f'[{get_datetime_format().split("_")[-1]}] - {name_donate}.py | Changed account Lulu{i}\n')
+            writer.write(delimtator)
+            change_city(i, writer)
+            writer.write(delimtator)
+            writer.write(f'[{get_datetime_format().split("_")[-1]}] - {name_donate}.py | Donate Alliance Techs Lulu{i}\n')
+            donate_alliance_tech(writer)
