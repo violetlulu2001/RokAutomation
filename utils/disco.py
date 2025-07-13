a9f47e1decab77c0398d2cfd029eb3872b717d3f
lulu <45490904+violetlulu2001@users.noreply.github.com>
2025-07-13 16:47:57 +0300

    fix intend if

--- utils/disco.py
+++ utils/disco.py
@@ -1,35 +1,16 @@
 import discord
 import os
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
-
-CHANNEL_ID = 1393861723647377528  # Your channel ID
-WATCH_DIR = r'D:\logs\violeta_logs'  # Folder to watch
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
-        self.channel = self.get_channel(CHANNEL_ID)
-
-class FileCreatedHandler(FileSystemEventHandler):
-    def __init__(self, client):
-        self.client = client
-
-    def on_created(self, event):
-        if not event.is_directory:
-            discord_file = discord.File(event.src_path)
-            coro = self.client.channel.send(file=discord_file)
-            discord.utils.run_coroutine_threadsafe(coro, self.client.loop)
+        channel = self.get_channel(1393861723647377528)  # Replace with your channel ID
+        file_path = r'D:\RokAutomation\logs\violeta_logs\your_file.txt'  # Update with your file path
+        if channel and os.path.exists(file_path):
+            await channel.send(file=discord.File(file_path))
 
 intents = discord.Intents.default()
 intents.message_content = True
+
 client = MyClient(intents=intents)
-
-def start_watcher():
-    event_handler = FileCreatedHandler(client)
-    observer = Observer()
-    observer.schedule(event_handler, WATCH_DIR, recursive=False)
-    observer.start()
-
-start_watcher()
 client.run(os.environ['discorbot'])
