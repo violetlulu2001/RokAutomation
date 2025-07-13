d7fbf4949f526aa232f7c7d36bd44d34f35ae830
violetlulu2001 <stroeandrei483@gmail.com>
2025-07-13 15:18:38 +0300

    fix intend if

--- donate_techs_rss.py
+++ donate_techs_rss.py
@@ -13,10 +13,10 @@ if __name__ == '__main__':
     path_file: str = os.path.join(path_file, name_donate)
 
     with open(f'{path_file}.txt', 'a') as writer:
-        for i in range(1, count_accounts+1):
-            writer.write(f'[{get_datetime_format().split("_")[-1]}] - {name_donate}.py | Changed account Lulu{i}\n')
-            writer.write(delimtator)
-            change_city(i, writer)
-            writer.write(delimtator)
-            writer.write(f'[{get_datetime_format().split("_")[-1]}] - {name_donate}.py | Donate Alliance Techs Lulu{i}\n')
-            donate_alliance_tech(writer)
+        # for i in range(1, count_accounts+1):
+        writer.write(f'[{get_datetime_format().split("_")[-1]}] - {name_donate}.py | Changed account Lulu{i}\n')
+        writer.write(delimtator)
+        # change_city(i, writer)
+        writer.write(delimtator)
+        writer.write(f'[{get_datetime_format().split("_")[-1]}] - {name_donate}.py | Donate Alliance Techs Lulu{i}\n')
+        donate_alliance_tech(writer)
