ea7f322abc113d11530ade0eba02e2453c09a417
lulu <45490904+violetlulu2001@users.noreply.github.com>
2025-07-13 17:01:41 +0300

    fix intend if

--- utils/disco.py
+++ utils/disco.py
@@ -4,10 +4,12 @@ import os
 class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
-        channel = self.get_channel(1393861723647377528)  # Replace with your channel ID
-        file_path = r'D:\RokAutomation\logs\violeta_logs\donate_techs_rss_163653653215.txt'  # Update with your file path
-        # if channel and os.path.exists(file_path):
-        await channel.send(file=discord.File(file_path))
+        channel = self.get_channel(1393861723647377528)
+        file_path = r'D:\RokAutomation\logs\violeta_logs\donate_techs_rss_163653653215.txt'
+        if channel and os.path.exists(file_path):
+            await channel.send(file=discord.File(file_path))
+        else:
+            print('Channel not found or file does not exist.')
 
 intents = discord.Intents.default()
 intents.message_content = True
