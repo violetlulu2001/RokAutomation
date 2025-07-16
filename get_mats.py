686e53a16ef93d701382650f5d2c584cad500752
lulu <45490904+violetlulu2001@users.noreply.github.com>
2025-07-16 23:52:12 +0300

    fix intend if

--- get_mats.py
+++ get_mats.py
@@ -1,9 +1,17 @@
+import os
 from utils.reader_json import accounts
-from utils.mouse_code import (change_city,
-                              start_blacksmith)
+from utils.mouse_code import (change_city, start_blacksmith)
+
+from utils.os_zone import os_file
+from utils.constants import name_donate, delimtator
+from utils.util_function import get_datetime_format
+
 
 if __name__ == 'get_mats':
     count_accounts = accounts["Accounts_count"]["first_acc"]
-    for i in range(1, count_accounts+1):
-        change_city(i)
-        start_blacksmith('bone')
+    path_file = os_file.create_folder()
+    path_file: str = os.path.join(path_file, name_donate)
+    with open(f'{path_file}_{get_datetime_format().split("_")[-1]}.txt', 'a') as writer:
+        for i in range(1, count_accounts+1):
+            change_city(i, writer)
+            start_blacksmith('bone')
