25ccbdd4fe7e99d00bb4671abb6b4677fbadf0b1
lulu <45490904+violetlulu2001@users.noreply.github.com>
2025-07-20 01:33:47 +0300

    modify

--- join_rally.py
+++ join_rally.py
@@ -1,13 +1,13 @@
 from utils.mouse_code import (change_city, join_rally, change_acct)
 
 if __name__ == '__main__':
-    # list_acc = [6,7]
-    # for i in list_acc:
-    #     change_city(int(i))
-    join_rally()
-    # change_acct()
-    # list_acc = [2, 3]
-    # for i in list_acc:
-    #     change_city(int(i))
-    #     join_rally()
-    # change_acct()
+    list_acc = [6,7]
+    for i in list_acc:
+        change_city(int(i))
+        join_rally()
+    change_acct()
+    list_acc = [2, 3]
+    for i in list_acc:
+        change_city(int(i))
+        join_rally()
+    change_acct()
