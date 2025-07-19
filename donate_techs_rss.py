889bc9d51846970d4963443072e134718fedd447
lulu <45490904+violetlulu2001@users.noreply.github.com>
2025-07-20 02:29:45 +0300

    modify

--- donate_techs_rss.py
+++ donate_techs_rss.py
@@ -4,7 +4,7 @@ from utils.reader_json import accounts
 from utils.mouse_code import (change_city, donate_alliance_tech)
 
 if __name__ == '__main__':
-    count_accounts = accounts["Accounts_count"]["first_acc"]
+    count_accounts = accounts["Accounts_count"]["second_acc"]
     for i in range(1, count_accounts+1):
         change_city(i)
         donate_alliance_tech()
