d7f8de604da8d47581fa70ed6eb009158b5362ca
lulu <45490904+violetlulu2001@users.noreply.github.com>
2025-07-20 10:17:50 +0300

    modify

--- get_mats.py
+++ get_mats.py
@@ -1,17 +1,9 @@
-import os
 from utils.reader_json import accounts
 from utils.mouse_code import (change_city, start_blacksmith)
-
-from utils.os_zone import os_file
-from utils.constants import name_mat, delimtator
-from utils.util_function import get_datetime_format
 
 
 if __name__ == '__main__':
     count_accounts = accounts["Accounts_count"]["first_acc"]
-    path_file = os_file.create_folder()
-    path_file: str = os.path.join(path_file, name_mat)
-    with open(f'{path_file}_{get_datetime_format().split("_")[-1]}.txt', 'a') as writer:
-        for i in range(1, count_accounts+1):
-            change_city(i, writer)
-            start_blacksmith('bone')
+    for i in range(1, count_accounts+1):
+        change_city(i)
+        start_blacksmith('bone')
