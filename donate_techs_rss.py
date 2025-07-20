d50e68746f5eff14ad5215c2b8e5c5db451adc2c
lulu <45490904+violetlulu2001@users.noreply.github.com>
2025-07-20 07:39:01 +0300

    modify

--- donate_techs_rss.py
+++ donate_techs_rss.py
@@ -1,10 +1,17 @@
 import time
 
 from utils.reader_json import accounts
-from utils.mouse_code import (change_city, donate_alliance_tech)
+from utils.mouse_code import (change_city, donate_alliance_tech, change_acct)
 
 if __name__ == '__main__':
     count_accounts = accounts["Accounts_count"]["second_acc"]
     for i in range(1, count_accounts+1):
         change_city(i)
         donate_alliance_tech()
+    change_acct()
+    count_accounts = accounts["Accounts_count"]["first_acc"]
+    for i in range(1, count_accounts + 1):
+        change_city(i)
+        donate_alliance_tech()
+    change_acct()
+
