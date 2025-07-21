5b8ab7e164206ce3f5573d833a7770dee167cf08
lulu <45490904+violetlulu2001@users.noreply.github.com>
2025-07-21 14:42:01 +0300

    modify

--- join_rally.py
+++ join_rally.py
@@ -3,8 +3,8 @@ import time
 from utils.mouse_code import (change_city, join_rally, change_acct)
 
 if __name__ == '__main__':
-    list_acc = [2, 3]
-    for i in list_acc:
-        change_city(int(i))
-        time.sleep(1)
-        join_rally()
+    # list_acc = [2, 3]
+    # for i in list_acc:
+    #     change_city(int(i))
+    #     time.sleep(1)
+    join_rally()
