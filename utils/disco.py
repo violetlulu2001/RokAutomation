fa4a631802751589ec4b59849e918e6c8109c59f
lulu <45490904+violetlulu2001@users.noreply.github.com>
2025-07-21 19:10:00 +0300

    modify

--- utils/disco.py
+++ utils/disco.py
@@ -2,14 +2,8 @@ import discord
 import os
 
 class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
-        channel = self.get_channel(1393861723647377528)
-        file_path = r'D:\logs\violeta_logs\donate_techs_rss_163653653215.txt'
-        if channel and os.path.exists(file_path):
-            await channel.send(file=discord.File(file_path))
-        else:
-            print('Channel not found or file does not exist.')
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
 
 intents = discord.Intents.default()
 intents.message_content = True
