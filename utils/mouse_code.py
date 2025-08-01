7adb844fc37440bbbd46f8db0d2e363e1b6dc20d
lulu <45490904+violetlulu2001@users.noreply.github.com>
2025-08-01 09:32:58 +0300

    modify

--- utils/mouse_code.py
+++ utils/mouse_code.py
@@ -37,10 +37,10 @@ def change_city(i:int) -> None:
     pyautogui.click(accounts[f'Bot{i}']['x'], accounts[f'Bot{i}']['y'], duration=2)
     pyautogui.click(setting['Account_changer']['yes_btn_change_acc']['x'],
                     setting['Account_changer']['yes_btn_change_acc']['y'], duration=2)
-    time.sleep(10)
+    time.sleep(20)
     pyautogui.click(setting['Account_changer']['yes_btn_change_acc']['x'],
                     setting['Account_changer']['yes_btn_change_acc']['y'], duration=2)
-    time.sleep(15)
+    time.sleep(20)
 
 def change_acct() -> None:
     pyautogui.click(setting['Profile']["x"], setting['Profile']["y"], duration=1)
