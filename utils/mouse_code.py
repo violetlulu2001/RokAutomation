e947fe089dceef1f5ec4d3092ff4c26f06a0e993
stroe <stroeandrei483@gmail.com>
2026-02-15 03:13:29 -0800

    Revert "add"
    This reverts commit 3f374824

--- utils/mouse_code.py
+++ utils/mouse_code.py
@@ -2,7 +2,7 @@ import os
 import time
 import pyautogui
 
-from utils.reader_json import accounts, setting, rightmenu, city, slow
+from reader_json import accounts, setting, rightmenu, city, slow
 
 
 def join_rally() -> None:
