a7e118e648dc20b9226d4b1470edb16f2ed81a96
stroe <stroeandrei483@gmail.com>
2026-06-15 22:18:29 -0700

    Add second bot

--- utils/YukAI.py
+++ utils/YukAI.py
@@ -5,7 +5,10 @@ import time
 from mouse_code import (send_slow_build_af, change_city, join_rally, change_acct, open_game, close_game)
 
 
+
 class MyClient(discord.Client):
+    server_mc_status = False
+
     async def on_message(self, message):
         if message.author == self.user:
             return  # Ignore messages from the bot itself
@@ -47,6 +50,32 @@ class MyClient(discord.Client):
             close_game()
             await message.channel.send('Game closed!')
 
+        if message.content.startswith("!start_mc"):
+            if not self.server_mc_status:
+                await message.channel.send('Starting Minecraft server...')
+                # Here you can add the code to start your Minecraft server
+                time.sleep(6)
+                await message.channel.send('starting port 62411')
+                time.sleep(2)
+                await message.channel.send('Minecraft server started!')
+                await message.channel.send("IP: 192.168.0.174:62411")
+                self.server_mc_status = True
+
+        if message.content.startswith("!status_mc"):
+            if self.server_mc_status:
+                await message.channel.send('Minecraft server is running.')
+            else:
+                await message.channel.send('Minecraft server is not running.')
+
+        if message.content.startswith("!stop_mc"):
+            if self.server_mc_status:
+                await message.channel.send('Stopping Minecraft server...')
+                # Here you can add the code to stop your Minecraft server
+                time.sleep(5)
+                await message.channel.send('kill port 62411')
+                time.sleep(2)
+                await message.channel.send('Minecraft server stopped!')
+                self.server_mc_status = False
 
 intents = discord.Intents.default()
 intents.message_content = True
